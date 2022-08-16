2d05b7199deeb5b9cf3134f3fcd8690b76549d24
Ooi Thian Hooi <63966221+ThianHooi@users.noreply.github.com>
2022-08-16 13:29:49 +0800

    Initial commit

---
+++ Makefile
@@ -0,0 +1,44 @@
+bootstrap:
+	yarn 
+
+
+# To compile the entire project, building your smart contracts
+compile: 
+	npx hardhat compile
+
+
+.PHONY: test
+test: 
+	npx hardhat test
+
+# verify:
+# 	npx hardhat verify --network mainnet DEPLOYED_CONTRACT_ADDRESS "Constructor argument 1"
+
+
+# To clean the cache and delete compiled smart contracts
+clean:
+	npx hardhat clean 
+
+prettier:
+	npx prettier --write 'contracts/**/*.sol'
+
+lint:
+	npx prettier --list-different 'contracts/**/*.sol'
+
+
+hhnet:
+	npx hardhat node
+
+gnet:
+	ganache -p 8545 --chain.chainId 31337 
+
+
+# To run a specific script in a specific network
+deploy_hhnet:
+	npx hardhat run scripts/deploy.ts --network localhost
+
+deploy_gnet:
+	npx hardhat run scripts/deploy.ts --network ganache
+
+deploy_glnet:
+	npx hardhat run scripts/deploy.ts --network goerli
---
+++ hardhat.config.ts
@@ -0,0 +1,50 @@
+import '@nomicfoundation/hardhat-toolbox';
+import '@nomiclabs/hardhat-solhint';
+import '@nomicfoundation/hardhat-chai-matchers';
+import '@typechain/hardhat';
+import '@nomiclabs/hardhat-ethers';
+import 'dotenv/config';
+
+import {HardhatUserConfig, task} from 'hardhat/config';
+
+// ref: https://stermi.medium.com/how-to-deploy-your-first-smart-contract-on-ethereum-with-solidity-and-hardhat-22f21d31096e
+task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: '0.8.9',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    localhost: {
+      url: 'http://127.0.0.1:8545',
+    },
+    // ref: https://gist.github.com/mingderwang/64046242aabff1e796ecaa4a93792fbd
+    // ref: https://hardhat.org/hardhat-runner/docs/config
+    ganache: {
+      url: 'http://127.0.0.1:7545',
+    },
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY || ''}`,
+      accounts: [process.env.GOERLI_PRIVATE_KEY || ''],
+    },
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+};
+
+export default config;
